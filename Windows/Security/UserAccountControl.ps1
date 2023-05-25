cbcf37a8792e6d5823c905d5b16c07ddb3b4bcb0
Yan Kuang <YTKme@Outlook.com>
2023-05-25 00:47:58 -0700

    Add UserAccountControl PowerShell script

---
+++ Windows/Security/UserAccountControl.ps1
@@ -0,0 +1,84 @@
+#
+# Filename: UserAccountControl.ps1
+#
+
+[CmdletBinding()]
+
+# Command Parameter
+param (
+    # Notify
+    [Parameter(Mandatory=$false)]
+    [ValidateSet('Default', 'Always', 'Dim', 'NoDim', 'Never')]
+    [string]$Notify
+)
+
+$ErrorActionPreference = 'Stop'
+
+$Filename = 'UserAccountControl.ps1'
+
+Write-Verbose "Entering $Filename"
+
+function Set-Notify {
+    [CmdletBinding(DefaultParameterSetName='Default')]
+    param(
+        [Parameter(Mandatory=$false, ParameterSetName='Default')]
+        [switch]$Default,
+        [Parameter(Mandatory=$false, ParameterSetName='Always')]
+        [switch]$Always,
+        [Parameter(Mandatory=$false, ParameterSetName='Dim')]
+        [switch]$Dim,
+        [Parameter(Mandatory=$false, ParameterSetName='NoDim')]
+        [switch]$NoDim,
+        [Parameter(Mandatory=$false, ParameterSetName='Never')]
+        [switch]$Never
+    )
+
+    function Set-Notify-Always {
+        Write-Host 'Set User Account Control Notify Always'
+        Write-Host 'Set User Account Control Notify Always Complete'
+    }
+
+    # Default
+    function Set-Notify-Dim {
+        Write-Host 'Set User Account Control Notify Dim'
+        Write-Host 'Set User Account Control Notify Dim Complete'
+    }
+
+    function Set-Notify-NoDim {
+        Write-Host 'Set User Account Control Notify No Dim'
+        Write-Host 'Set User Account Control Notify No Dim Complete'
+    }
+
+    function Set-Notify-Never {
+        Write-Host 'Set User Account Control Notify Never'
+        # Set-ItemProperty `
+        # -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' `
+        # -Name 'ConsentPromptBehaviorAdmin' `
+        # -Value 0
+        # Set-ItemProperty `
+        # -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' `
+        # -Name 'PromptOnSecureDesktop' `
+        # -Value 0
+        Write-Host 'Set User Account Control Notify Never Complete'
+    }
+
+    if ($Default) {
+        Set-Notify-Dim
+    } elseif ($Always) {
+        Set-Notify-Always
+    } elseif ($Dim) {
+        Set-Notify-Dim
+    } elseif ($NoDim) {
+        Set-Notify-NoDim
+    } elseif ($Never) {
+        Set-Notify-Never
+    }
+}
+
+# User Account Control Notify
+if ($PSBoundParameters.ContainsKey('Notify')) {
+    $NotifyParameter = @{ $Notify = $true }
+    Set-Notify @NotifyParameter
+}
+
+Write-Verbose "Leaving $Filename"
