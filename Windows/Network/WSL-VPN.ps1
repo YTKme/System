607fd3b7b6f1b6a4a82d128198482d5c6b8c447b
Yan Kuang <YTKme@Outlook.com>
2023-06-21 17:24:26 -0700

    Add WSL-VPN PowerShell script

---
+++ Windows/Network/WSL-VPN.ps1
@@ -0,0 +1,5 @@
+# 
+
+# Source: https://live.paloaltonetworks.com/t5/globalprotect-discussions/globalprotect-blocks-the-network-traffic-of-wsl2/m-p/507821/highlight/true#M2955
+
+C:\Windows\System32\wsl.exe --exec /bin/bash --noprofile --norc -c "/bin/ip -o -4 addr list eth0"
