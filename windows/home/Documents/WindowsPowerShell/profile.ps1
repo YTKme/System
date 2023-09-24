4142c1ce88ddf1a3e1fe7b87af8bfde4d1c15bde
Yan Kuang <YTKme@Outlook.com>
2023-09-23 23:20:55 -0700

    Update profile PowerShell script

--- windows/home/Documents/WindowsPowerShell/profile.ps1
+++ windows/home/Documents/WindowsPowerShell/profile.ps1
@@ -13,8 +13,42 @@ Write-Host "Start Profile: $Filename" -ForegroundColor Cyan
 # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 
 # Import `posh-git` Module
-Write-Host 'Execute: Import-Module posh-git'
-Import-Module posh-git
+# Write-Verbose 'Module: posh-git'
+# Import-Module posh-git
+
+#########
+# Alias #
+#########
+
+# WSL VPN
+# Write-Verbose 'Alias: wslvpn'
+# New-Alias -Name wslvpn -Value "$env:USERPROFILE\Documents\WindowsPowerShell\WSL-VPN.ps1"
+
+# Change Directory
+Write-Verbose 'Alias: cdrepository'
+New-Alias -Name cdrepository -Value Set-Location-Repository
+
+# Git Configuration
+Write-Verbose 'Alias: gcself'
+New-Alias -Name gcself -Value Set-Git-Configuration-self
+
+############
+# Function #
+############
+
+# Change Directory
+# Write-Verbose 'Function: Set-Location-Repository'
+# Function Set-Location-Repository {
+#     Set-Location -Path $env:USERPROFILE\Desktop\Project\Repository\
+# }
+
+# Git Configuration
+Write-Verbose 'Function: Set-Git-Configuration-self'
+Function Set-Git-Configuration-self {
+    git config core.filemode false
+    git config user.email "email@localhost.lcl"
+    git config user.name "Self"
+}
 
 Write-Host "End Profile: $Filename" -ForegroundColor Cyan
 Write-Verbose "Leaving: $Filename"
