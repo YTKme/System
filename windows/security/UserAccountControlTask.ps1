65ae77eed6c8ee05cacca33c099eed70acfd5541
Yan Kuang <YTKme@Outlook.com>
2023-09-24 15:07:55 -0700

    Update UserAccountControlTask PowerShell script with Task Scheduler folder create and delete

--- windows/security/UserAccountControlTask.ps1
+++ windows/security/UserAccountControlTask.ps1
@@ -8,7 +8,7 @@
 param (
     # Action
     [Parameter(Mandatory=$false)]
-    [ValidateSet('Create', 'Delete')]
+    [ValidateSet('New', 'Remove')]
     [string]$Action
 )
 
@@ -23,28 +23,55 @@ $Hostname = [System.Net.Dns]::GetHostName()
 
 # Create Schedule Service Object
 $ScheduleServiceObject = New-Object -ComObject schedule.service
+# Connect Schedule Service Object
+$ScheduleServiceObject.connect()
 
-# Create Task Scheduler Folder
-function New-Task-Scheduler-Folder {
-    $ScheduleServiceObject.connect()
-    $RootFolder = $ScheduleServiceObject.GetFolder('\')
-    $RootFolder.CreateFolder($Hostname)
+function Set-Task {
+    [CmdletBinding(DefaultParameterSetName='New')]
+    param(
+        [Parameter(Mandatory=$false, ParameterSetName='New')]
+        [switch]$New,
+        [Parameter(Mandatory=$false, ParameterSetName='Remove')]
+        [switch]$Remove
+    )
+
+    # New Task Scheduler Folder
+    function New-Task-Scheduler-Folder {
+        Write-Host "New Task Scheduler Folder" -ForegroundColor Cyan
+        $RootFolder = $ScheduleServiceObject.GetFolder('\')
+        $RootFolder.CreateFolder($Hostname)
+        Write-Host "New Task Scheduler Folder Complete" -ForegroundColor Green
+    }
+
+    # Remove Task Scheduler Folder
+    function Remove-Task-Scheduler-Folder {
+        Write-Host "Remove Task Scheduler Folder" -ForegroundColor Cyan
+        $RootFolder = $ScheduleServiceObject.GetFolder('\')
+        $RootFolder.DeleteFolder($Hostname, $null)
+        Write-Host "Remove Task Scheduler Folder Complete" -ForegroundColor Green
+    }
+
+    # New User Account Control Task
+    function New-User-Account-Control-Task {
+        New-Task-Scheduler-Folder
+    }
+
+    # Remove User Account Control Task
+    function Remove-User-Account-Control-Task {
+        Remove-Task-Scheduler-Folder
+    }
+
+    if ($New) {
+        New-User-Account-Control-Task
+    } elseif ($Remove) {
+        Remove-User-Account-Control-Task
+    }
 }
 
-# Delete Task Scheduler Folder
-function Remove-Task-Scheduler--Folder {
-
+# Execute User Account Control Task
+if ($PSBoundParameters.ContainsKey('Action')) {
+    $ActionParameter = @{ $Action = $true }
+    Set-Task @ActionParameter
 }
-
-# Create User Account Control Task
-function New-User-Account-Control-Task {
-
-}
-
-function Remove-User-Account-Control-Task {
-
-}
-
-# Delete User Account Control Task
 
 Write-Verbose "Leaving $Filename"
