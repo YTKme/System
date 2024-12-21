2fb11763e2f0241d9fcb823f7baadfc61c2bada2
Yan Kuang <YTKme@Outlook.com>
2024-12-21 11:01:52 -0800

    Update Windows PowerShell profile script

--- windows/home/Documents/WindowsPowerShell/profile.ps1
+++ windows/home/Documents/WindowsPowerShell/profile.ps1
@@ -20,6 +20,10 @@ Write-Host "Start Profile: $Filename" -ForegroundColor Cyan
 # Alias #
 #########
 
+# Profile
+Write-Verbose 'Alias: newdesktop'
+New-Alias -Name newdesktop -Value New-Desktop
+
 # WSL VPN
 # Write-Verbose 'Alias: wslvpn'
 # New-Alias -Name wslvpn -Value "$env:USERPROFILE\Documents\WindowsPowerShell\WSL-VPN.ps1"
@@ -31,10 +35,37 @@ New-Alias -Name cdrepository -Value Set-Location-Repository
 # Git Configuration
 Write-Verbose 'Alias: gcself'
 New-Alias -Name gcself -Value Set-Git-Configuration-self
+Write-Verbose 'Alias: glaogd'
+New-Alias -Name glaogd -Value Get-Git-Log-All-Oneline-Graph-Decorate
 
 ############
 # Function #
 ############
+
+# New Desktop
+Write-Verbose 'Function: New-Desktop'
+function New-Desktop {
+    $StartingDirectory = "$env:USERPROFILE\Desktop"
+    $ArgumentList = @(
+        "--window 0",
+        "new-tab",
+        "--title `"Desktop`"",
+        "--profile `"Windows PowerShell`"",
+        "--startingDirectory `"$StartingDirectory`";",
+        "split-pane --vertical --duplicate;",
+        "split-pane --horizontal --duplicate;",
+        "move-focus first"
+    ) -join ' '
+    Start-Process wt -ArgumentList $ArgumentList
+    # wt `
+    #     --window 0 new-tab `
+    #     --title "Desktop" `
+    #     --profile "Windows PowerShell" `
+    #     --startingDirectory "$env:USERPROFILE\Desktop" `
+    #     split-pane --vertical --duplicate `
+    #     split-pane --horizontal --duplicate `
+    #     move-focus first
+}
 
 # Change Directory
 # Write-Verbose 'Function: Set-Location-Repository'
@@ -50,6 +81,11 @@ Function Set-Git-Configuration-self {
     git config user.name "Self"
     git config core.eol lf
     git config core.autocrlf input
+}
+
+Write-Verbose 'Function: Get-Git-Log-All-Oneline-Graph-Decorate'
+Function Get-Git-Log-All-Oneline-Graph-Decorate {
+    git log --all --oneline --graph --decorate
 }
 
 Write-Host "End Profile: $Filename" -ForegroundColor Cyan
